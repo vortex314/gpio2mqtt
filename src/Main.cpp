aa0b51f05b6b05cc5c4d0f89d3223b4657c7737e
vortex314 <lieven.merckx@gmail.com>
2020-12-25 23:40:44 +0000

    all gpio

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,11 +9,13 @@
 Log logger(2048);
 Thread mainThread("main");
 MqttPaho mqtt(mainThread);
-Gpio gpio25(25);
 StaticJsonDocument<10240> jsonDoc;
+
+uint8_t gpioRaspberry[] = {4, 9, 10, 17, 18, 22, 23, 24, 25, 27};
 
 int main(int argc, char** argv) {
   Sys::init();
+  Gpio::init();
 
   JsonObject mqttConfig = jsonDoc.to<JsonObject>();
   mqttConfig["device"] = "GPIO";
@@ -22,13 +24,26 @@ int main(int argc, char** argv) {
   mqtt.init();
   mqtt.connect();
 
-  gpio25.mode.on("OUTPUT");
+  for (uint32_t i = 0; i < sizeof(gpioRaspberry); i++) {
+    uint32_t gpioIdx = gpioRaspberry[i];
+    Gpio* gpio = new Gpio(mainThread, gpioIdx);
+    gpio->mode.on("INPUT");
+    std::string gpioValue = "gpio" + std::to_string(gpioIdx) + "/value";
+    std::string gpioMode = "gpio" + std::to_string(gpioIdx) + "/mode";
 
-  // mqtt.fromTopic<int>("gpio25/value") >> gpio25.value;
-
-  mqtt.topic<int>("gpio25/value") == gpio25.value;
-
-  mqtt.topic<std::string>("gpio25/mode") == gpio25.mode;
+    mqtt.fromTopic<int>(gpioValue.c_str()) >> gpio->value;
+    mqtt.fromTopic<std::string>(gpioMode.c_str()) >> gpio->mode;
+    gpio->value >> Cache<int>::nw(mainThread, 100, 1000) >>
+        mqtt.toTopic<int>(gpioValue.c_str());
+    gpio->mode >> Cache<std::string>::nw(mainThread, 100, 10000) >>
+        mqtt.toTopic<std::string>(gpioMode.c_str());
+    gpio->mode.on("INPUT");
+    gpio->value.on(0);
+    if (gpioIdx == 25) {
+      gpio->mode.on("OUTPUT");
+      gpio->value.on(1);
+    }
+  }
 
   mqtt.fromTopic<int>("src/pcdell/js0/axis0") >>
       mqtt.toTopic<int>("dst/GPIO/gpio25/value");
