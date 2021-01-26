7a7982322801be30a8a13e85db64799b6ff16ed1
vortex314 <lieven.merckx@gmail.com>
2021-01-26 23:06:59 +0000

    added initialization to gpio via config file

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,16 +10,48 @@ Log logger(2048);
 Thread mainThread("main");
 MqttPaho mqtt(mainThread);
 StaticJsonDocument<10240> jsonDoc;
+/*
+  load configuration file into JsonObject
+*/
+bool loadConfig(JsonDocument& doc, const char* name) {
+  FILE* f = fopen(name, "rb");
+  if (f == NULL) {
+    ERROR(" cannot open config file : %s", name);
+    return false;
+  }
+  fseek(f, 0, SEEK_END);
+  long fsize = ftell(f);
+  fseek(f, 0, SEEK_SET); /* same as rewind(f); */
 
-uint8_t gpioRaspberry[] = {4, 9, 10, 17, 18, 22, 23, 24, 25, 27};
+  char* string = (char*)malloc(fsize + 1);
+  fread(string, 1, fsize, f);
+  fclose(f);
+
+  string[fsize] = 0;
+
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, string);
+  if (error) {
+    ERROR(" JSON parsing config file : %s failed.", name);
+    return false;
+  }
+  return true;
+}
+
+// uint8_t gpioRaspberry[] = {4, 9, 10, 17, 18, 22, 23, 24, 25, 27}; //
+// RAspberry Pi 1
+uint8_t gpioRaspberry[] = {2, 4, 9, 10, 17, 18, 22, 23, 24, 25, 26, 27};
 
 int main(int argc, char** argv) {
   Sys::init();
   Gpio::init();
+  if (loadConfig(jsonDoc, "gpio2mqtt.json")) {
+    std::string jsonString;
+    serializeJsonPretty(jsonDoc, jsonString);
+    INFO(" config loaded : %s ", jsonString.c_str());
+  }
 
-  JsonObject mqttConfig = jsonDoc.to<JsonObject>();
-  mqttConfig["device"] = "GPIO";
-  mqttConfig["connection"] = "tcp://limero.ddns.net";
+  JsonObject mqttConfig = jsonDoc["mqtt"];
   mqtt.config(mqttConfig);
   mqtt.init();
   mqtt.connect();
@@ -27,7 +59,6 @@ int main(int argc, char** argv) {
   for (uint32_t i = 0; i < sizeof(gpioRaspberry); i++) {
     uint32_t gpioIdx = gpioRaspberry[i];
     Gpio* gpio = new Gpio(mainThread, gpioIdx);
-    gpio->mode.on("INPUT");
     std::string gpioValue = "gpio" + std::to_string(gpioIdx) + "/value";
     std::string gpioMode = "gpio" + std::to_string(gpioIdx) + "/mode";
 
@@ -37,11 +68,11 @@ int main(int argc, char** argv) {
         mqtt.toTopic<int>(gpioValue.c_str());
     gpio->mode >> Cache<std::string>::nw(mainThread, 100, 10000) >>
         mqtt.toTopic<std::string>(gpioMode.c_str());
-    gpio->mode.on("INPUT");
-    gpio->value.on(0);
-    if (gpioIdx == 25) {
-      gpio->mode.on("OUTPUT");
-      gpio->value.on(1);
+
+    JsonArray gpioConfig = jsonDoc["gpio"][std::to_string(gpioIdx)];
+    if (gpioConfig) {
+      gpio->mode.on(gpioConfig[0]);
+      gpio->value.on(gpioConfig[1]);
     }
   }
 
