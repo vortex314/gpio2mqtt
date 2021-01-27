a73a12462bb9f63b67d723ef39a9c991269860f7
vortex314 <lieven.merckx@gmail.com>
2021-01-27 22:45:24 +0000

    cleaner initialization

--- src/Main.cpp
+++ src/Main.cpp
@@ -38,9 +38,38 @@ bool loadConfig(JsonDocument& doc, const char* name) {
   return true;
 }
 
+/*
++-----+-----+---------+------+---+---Pi 3B--+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ |     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
+ |   2 |   8 |   SDA.1 |  OUT | 1 |  3 || 4  |   |      | 5v      |     |     |
+ |   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
+ |   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 0 | IN   | TxD     | 15  | 14  |
+ |     |     |      0v |      |   |  9 || 10 | 1 | IN   | RxD     | 16  | 15  |
+ |  17 |   0 | GPIO. 0 |   IN | 0 | 11 || 12 | 0 | IN   | GPIO. 1 | 1   | 18  |
+ |  27 |   2 | GPIO. 2 |  OUT | 1 | 13 || 14 |   |      | 0v      |     |     |
+ |  22 |   3 | GPIO. 3 |  OUT | 0 | 15 || 16 | 0 | IN   | GPIO. 4 | 4   | 23  |
+ |     |     |    3.3v |      |   | 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |
+ |  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |
+ |   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 1 | IN   | GPIO. 6 | 6   | 25  |
+ |  11 |  14 |    SCLK | ALT0 | 0 | 23 || 24 | 1 | OUT  | CE0     | 10  | 8   |
+ |     |     |      0v |      |   | 25 || 26 | 1 | OUT  | CE1     | 11  | 7   |
+ |   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
+ |   5 |  21 | GPIO.21 |   IN | 1 | 29 || 30 |   |      | 0v      |     |     |
+ |   6 |  22 | GPIO.22 |  OUT | 0 | 31 || 32 | 0 | OUT  | GPIO.26 | 26  | 12  |
+ |  13 |  23 | GPIO.23 |   IN | 0 | 33 || 34 |   |      | 0v      |     |     |
+ |  19 |  24 | GPIO.24 |   IN | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
+ |  26 |  25 | GPIO.25 |  OUT | 1 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |
+ |     |     |      0v |      |   | 39 || 40 | 0 | IN   | GPIO.29 | 29  | 21  |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+---Pi 3B--+---+------+---------+-----+-----+
+*/
+
 // uint8_t gpioRaspberry[] = {4, 9, 10, 17, 18, 22, 23, 24, 25, 27}; //
 // RAspberry Pi 1
-uint8_t gpioRaspberry[] = {2, 4, 9, 10, 17, 18, 22, 23, 24, 25, 26, 27};
+//uint8_t gpioRaspberry[] = {0,1,2,3,4,5,6,7,21,22,23,24,25,26,27,28,29}; // wiringPi convention
 
 int main(int argc, char** argv) {
   Sys::init();
@@ -56,14 +85,15 @@ int main(int argc, char** argv) {
   mqtt.init();
   mqtt.connect();
 
-  for (uint32_t i = 0; i < sizeof(gpioRaspberry); i++) {
-    uint32_t gpioIdx = gpioRaspberry[i];
+  for (uint32_t i = 0; i < Gpio::raspberryGpio.size(); i++) {
+    uint32_t gpioIdx = Gpio::raspberryGpio[i];
     Gpio* gpio = new Gpio(mainThread, gpioIdx);
     std::string gpioValue = "gpio" + std::to_string(gpioIdx) + "/value";
     std::string gpioMode = "gpio" + std::to_string(gpioIdx) + "/mode";
 
     mqtt.fromTopic<int>(gpioValue.c_str()) >> gpio->value;
     mqtt.fromTopic<std::string>(gpioMode.c_str()) >> gpio->mode;
+
     gpio->value >> Cache<int>::nw(mainThread, 100, 1000) >>
         mqtt.toTopic<int>(gpioValue.c_str());
     gpio->mode >> Cache<std::string>::nw(mainThread, 100, 10000) >>
@@ -73,14 +103,10 @@ int main(int argc, char** argv) {
     if (gpioConfig) {
       gpio->mode.on(gpioConfig[0]);
       gpio->value.on(gpioConfig[1]);
+    } else {
+      gpio->mode.on("INPUT");
     }
   }
 
-  mqtt.fromTopic<int>("src/pcdell/js0/axis0") >>
-      mqtt.toTopic<int>("dst/GPIO/gpio25/value");
-
-  mqtt.connected >> [](const bool& b) {
-    if (b) mqtt.subscribe("src/pcdell/js0/axis0");
-  };
   mainThread.run();
 }
